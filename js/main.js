5a02c2e5447d8aca306438697b0fc3c854f4feca
abaalfadeel <abaalfadeel1@gmail.com>
2025-12-07 11:18:12 +0300

    Implement game loading and rendering logic
    Added game initialization and rendering functions.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+loadImages(ASSETS.images, (images)=>{
+  player = new Player(images.soldier);
+  // load audio etc
+  startGame();
+});
 
+function update(dt){
+  // TODO: spawn zombies, move them, collisions
+}
+function render(){
+  ctx.clearRect(0,0,WIDTH,HEIGHT);
+  // draw lanes
+  ctx.fillStyle='#222';
+  ctx.fillRect(WIDTH*0.5-2,0,4,HEIGHT);
+  player.draw(ctx);
+  // draw zombies, pickups...
+  document.getElementById('score').innerText = player.coins;
+}
+
+function playerAttack(){
+  player.attack();
+}
